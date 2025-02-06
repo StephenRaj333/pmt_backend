afb790f79e57355476c0a33cd817a8bddca4b361
Stephen Raj V <stephen@m2pfintech.com>
2025-02-06 22:03:17 +0530

    backend validation aded

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required:[true, "Name is Required"]},  
-    email: {type: String, required: [true, "Email is Required"],unique: true,}, 
-    password : {type: String, required: [true, "Password is Required"]} 
-})
+    name: {
+        type: String,
+        required: [true, "Name is Required"],
+        minlength: [4, "Name must be at least 4 characters long"],
+        maxlength: [30, "Name must be at most 30 characters long"],
+        match: [/^[A-Za-z\s]+$/, "Name can only contain letters and spaces"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is Required"],
+        unique: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email format"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is Required"],
+    }
+});
+
 
 module.exports = mongoose.model("users",userSchema); 
